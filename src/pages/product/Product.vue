aeead9630524cae81e31b15104f302491c9cf091
cmk86 <poppli@naver.com>
2026-01-18 17:28:41 +0900

    save

--- src/pages/product/Product.vue
+++ src/pages/product/Product.vue
@@ -52,7 +52,7 @@ const products = [
     name: '폴 샤워기(Standard)',
     image: 'images/product/2인용폴.jpg',
     summary: '야외 설치에 적합한 자립형 폴 2인용 샤워기',
-    description: '야외 설치에 적합한 자립형 폴 2인용 샤워기입니다. 해수욕장·야영장 등 외부 환경에 적합합니다.1',
+    description: '야외 설치에 적합한 자립형 폴 2인용 샤워기입니다. 해수욕장·야영장 등 외부 환경에 적합합니다.',
     specs: [
       { label: '규격', value: '190x190x2420H' },
       { label: '소재', value: 'STS304 2.0t+정전분체도장' },
